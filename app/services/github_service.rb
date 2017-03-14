1bbedf4dfb4e4cdd7cc1000a63749267d0af6d4a
BrunoBoehm <bruno.boehm@gmail.com>
2017-03-14 21:11:15 +0100

    Done.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -5,6 +5,7 @@ class GithubService
   def initialize(access_hash = nil)
     @access_token = access_hash["access_token"] if access_hash
   end
+  # once GithubService.new(hash) we can call all instance methods
 
   def authenticate!(client_id, client_secret, code)
     response = Faraday.post "https://github.com/login/oauth/access_token",
